27033805e419d0d76321e38377f216fb219748e0
Arthur Axel 'fREW' Schmidt <frioux@gmail.com>
2015-05-28 22:27:28 -0500

    WIP

--- lib/API.pm
+++ lib/API.pm
@@ -5,91 +5,23 @@ use Moo;
 
 use experimental 'postderef';
 
-use AWS::Signature4;
-use Capture::Tiny 'capture';
+use VM::EC2;
 use Future;
 use Future::Utils 'repeat';
-use HTTP::Request;
-use HTTP::Request::Common 'POST';
-use JSON;
-use Net::Async::HTTP;
 use Try::Tiny;
-use URI;
-use XML::Simple 'XMLin';
 
-has _signer => (
+
+has _ec2 => (
    is => 'ro',
    lazy => 1,
    builder => sub {
-       AWS::Signature4->new(
-          -access_key => 'AKIAIX4VB7VY7DHWNYCA',
-          -secret_key => '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',
-       );
-   },
-   handles => {
-      _sign => 'sign',
-   },
-);
-
-has _loop => (
-   is => 'ro',
-   init_arg => 'loop',
-   required => 1,
-   handles => {
-      _add_to_loop => 'add',
-      _remove_from_loop => 'remove',
-      _delay => 'delay_future',
-   },
-);
-
-has _ua => (
-   is => 'ro',
-   lazy => 1,
-   handles => {
-      _do_request => 'do_request',
-   },
-   builder => sub {
-      my $self = shift;
-
-      my $ua = Net::Async::HTTP->new(
-         fail_on_error => 1,
+      VM::EC2->new(
+         -access_key => 'AKIAIX4VB7VY7DHWNYCA',
+         -secret_key => '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',
+         -endpoint   => 'https://ec2.us-west-2.amazonaws.com',
       );
-
-      $self->_add_to_loop($ua);
-
-      $ua
    },
 );
-
-sub _aws_request {
-   my ($self, $request) = @_;
-
-   $self->_sign($request);
-   $self->_do_request(
-      request => $request,
-   )
-}
-
-sub create_key_pair {
-   my $name = shift;
-   decode_json(capture {
-      system qw( aws ec2 create-key-pair --key-name), $name
-   });
-}
-
-sub _describe_instances {
-   my ($self, $args) = @_;
-
-   $self->_aws_request(
-      POST 'https://ec2.us-west-2.amazonaws.com/',
-      content_type => 'application/x-www-form-urlencoded',
-      Content => [
-         Action => 'DescribeInstances',
-         Version => '2015-04-15',
-         %$args,
-      ],
-   )
-}
 
 sub describe_instances {
    shift->_describe_instances(@_)->then(sub {
@@ -124,12 +56,6 @@ sub instance_ips {
 
       scalar @ips == scalar grep $_, @ips
    };
-}
-
-sub DEMOLISH {
-   my $self = shift;
-
-   $self->_remove_from_loop($self->_ua)
 }
 
 1;
